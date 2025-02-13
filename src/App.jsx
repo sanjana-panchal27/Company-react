a060f0cafc0d8bc8894dd4172a73315ffb47de22
Hackathon-2023 <sanjanapanchal2713@gmail.com>
2025-02-13 21:18:22 +0530

    added sidebar, navbar

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Navbar from "./Company/components/Navbar";
+import Sidebar from "./Company/components/Sidebar";
+
+function App() {
+  return (
+    <>
+      <title>Company Dashboard</title>
+      <Sidebar />
+      <Navbar />
+    </>
+  );
+}
+
+export default App;
